6f3fd1b638d57351a0992dc1b2b16b0fd63ff5d7
Al Hakim 1988 <116581267+Alhakim88@users.noreply.github.com>
2026-01-29 19:34:19 +0700

    Refactor guessing game to use multiple options

--- Tugas 4b.py
+++ Tugas 4b.py
@@ -1,10 +1,31 @@
-kata_rahasia = "python"
+import random
+
+kata_rahasia = {
+    1: "Python",
+    2: "Java",
+    3: "Swift",
+    4: "Kotlin"
+}
+
+jawaban_benar = random.randint(1, 4)
 
 while True:
-    tebakan = input("Tebak kata rahasianya: ")
+    print("\nPilih kata rahasia:")
+    print("1. Python")
+    print("2. Java")
+    print("3. Swift")
+    print("4. Kotlin")
 
-    if tebakan == kata_rahasia:
-        print("Kamu benar!")
-        break
-    else:
-        print("Salah! Coba lagi.")
+    try:
+        pilihan = int(input("Masukkan pilihan (1-4): "))
+
+        if pilihan == jawaban_benar:
+            print("Kamu benar! ")
+            break
+        elif pilihan in kata_rahasia:
+            print("Salah! Coba lagi.")
+        else:
+            print("Pilihan tidak valid!")
+
+    except ValueError:
+        print("Masukkan angka saja!")
